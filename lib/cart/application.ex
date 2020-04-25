fff46e013442204b93c19c3ea1b2101b72cdf66f
mmmries <riesmmm@gmail.com>
2020-04-25 13:27:17 -0600

    configure access point

--- lib/cart/application.ex
+++ lib/cart/application.ex
@@ -6,6 +6,7 @@ defmodule Cart.Application do
   use Application
 
   def start(_type, _args) do
+    setup_network(target())
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Cart.Supervisor]
@@ -34,6 +35,32 @@ defmodule Cart.Application do
       # Starts a worker by calling: Cart.Worker.start_link(arg)
       # {Cart.Worker, arg},
     ]
+  end
+
+  def setup_network(:host), do: nil
+
+  def setup_network(_) do
+    VintageNet.configure("wlan0", %{
+      type: VintageNetWiFi,
+      vintage_net_wifi: %{
+        networks: [
+          %{
+            mode: :ap,
+            ssid: "cart",
+            key_mgmt: :none
+          }
+        ]
+      },
+      ipv4: %{
+        method: :static,
+        address: "192.168.24.1",
+        netmask: "255.255.255.0"
+      },
+      dhcpd: %{
+        start: "192.168.24.2",
+        end: "192.168.24.10"
+      }
+    })
   end
 
   def target() do
